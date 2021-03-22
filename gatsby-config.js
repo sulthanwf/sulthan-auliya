418c487de2d0f003626ae4e34272c21215fa1f1f
sulthan <sulthanwf@gmail.com>
2021-03-23 01:04:24 +1300

    rollback to previous version

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `/sa-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/sa-icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
