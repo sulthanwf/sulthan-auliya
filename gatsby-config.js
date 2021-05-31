bd288fc8e676a820930a73f20ce97aeddc98a633
sulthan <sulthanwf@gmail.com>
2021-06-01 02:21:02 +1200

    modify gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,15 +40,9 @@ module.exports = {
       resolve: "gatsby-plugin-firebase",
       options: {
         credentials: {
-          apiKey: "AIzaSyBSZX7euFaAJR6dqfAgabrKNXtz5NUQCmY",
-          authDomain: "sulthanauliya-93b5b.firebaseapp.com",
-          projectId: "sulthanauliya-93b5b",
-          storageBucket: "sulthanauliya-93b5b.appspot.com",
-          messagingSenderId: "928005354940",
-          appId: "1:928005354940:web:170e9e9493bf2f2c19e7b9",
-          measurementId: "G-JGZ3032DSX"
-        }
-      }
+          //Your API Key
+        },
+      },
     },
     `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
