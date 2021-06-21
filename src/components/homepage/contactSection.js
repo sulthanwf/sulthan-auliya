45ea46aacdcef6f256ed11a457b93aa01af45f71
sulthan <sulthanwf@gmail.com>
2021-06-21 15:59:34 +1200

    simplicise the contact section

--- src/components/homepage/contactSection.js
+++ src/components/homepage/contactSection.js
@@ -1,37 +1,16 @@
 import React, { useState } from "react"
-import { graphql, useStaticQuery } from "gatsby"
 import styled from "styled-components"
 import { Title } from "../Title"
-import BackgroundImage from "gatsby-background-image"
 import firebase from "gatsby-plugin-firebase"
 import { ButtonB } from "../Button"
-import MsgBox from "../msgBox"
 
 const ContactSection = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      file(relativePath: { eq: "contact-bg.jpg" }) {
-        childImageSharp {
-          fluid(quality: 100, maxWidth: 1920) {
-            ...GatsbyImageSharpFluid_withWebp
-          }
-        }
-      }
-    }
-  `)
-  const contactBg = data.file.childImageSharp.fluid
-
   const [email, setEmail] = useState("")
   const [message, setMessage] = useState("")
   const timestamp = new Date()
 
   const [validEmail, setValidEmail] = useState([])
   const [validMessage, setValidMessage] = useState("")
-  const [msgBox, setMsgBox] = useState(true)
-
-  const toggleMsgBox = () => {
-    setMsgBox(!msgBox)
-  }
 
   //Form Validation
   const validateInput = () => {
@@ -78,7 +57,7 @@ const ContactSection = () => {
           timestamp: timestamp,
         })
         .then(() => {
-          toggleMsgBox()
+          alert("Your message has been sent")
         })
         .catch(error => {
           alert(error.message)
@@ -90,57 +69,47 @@ const ContactSection = () => {
   }
 
   return (
-    <BackgroundImage
-      Tag="div"
-      fluid={contactBg}
-      css={`
-        height: 100vh;
-      `}
-    >
-      <ContactContainer id="contact">
-        <ContactContent onSubmit={handleSubmit}>
-          <ContactTitle>
-            <Title>Get in Touch</Title>
-          </ContactTitle>
-          <ContactForm>
-            <input
-              type="email"
-              placeholder="Your email"
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-            />
-            <ValidationMsg className={validEmail[0]}>
-              {validEmail[1]}
-            </ValidationMsg>
-            <textarea
-              placeholder="Send your message"
-              value={message}
-              onChange={e => setMessage(e.target.value)}
-            />
-            <ValidationMsg className={validMessage[0]}>
-              {validMessage[1]}
-            </ValidationMsg>
-          </ContactForm>
-          <BtnBorder>
-            <ButtonB type="submit">Send</ButtonB>
-          </BtnBorder>
-        </ContactContent>
-
-        {msgBox ? <MsgBox show={msgBox} toggle={toggleMsgBox} /> : null}
-      </ContactContainer>
-    </BackgroundImage>
+    <ContactContainer id="contact">
+      <ContactContent onSubmit={handleSubmit}>
+        <ContactTitle>
+          <Title>Get in Touch</Title>
+        </ContactTitle>
+        <ContactForm>
+          <input
+            type="email"
+            placeholder="Your email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <ValidationMsg className={validEmail[0]}>
+            {validEmail[1]}
+          </ValidationMsg>
+          <textarea
+            placeholder="Send your message"
+            value={message}
+            onChange={e => setMessage(e.target.value)}
+          />
+          <ValidationMsg className={validMessage[0]}>
+            {validMessage[1]}
+          </ValidationMsg>
+        </ContactForm>
+        <BtnBorder>
+          <ButtonB type="submit">Send</ButtonB>
+        </BtnBorder>
+      </ContactContent>
+    </ContactContainer>
   )
 }
 
 export default ContactSection
 
 export const ContactContainer = styled.div`
-  background: none;
+  background: #fa7d09;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 100%;
+  height: 100vh;
   padding: 0.5rem calc((100vw - 690px) / 2);
   position: relative;
   color: #fff;
@@ -151,22 +120,6 @@ export const ContactContainer = styled.div`
 
   @media screen and (min-width: 1024px) {
     padding-bottom: 5rem;
-  }
-
-  ::before {
-    content: "";
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    right: 0;
-    left: 0;
-    z-index: 2;
-    background: linear-gradient(
-        180deg,
-        rgba(0, 0, 0, 0.2) 100%,
-        rgba(0, 0, 0, 0.2) 0%
-      ),
-      linear-gradient(180deg, rgba(0, 0, 0, 0.2) 0%, transparent 100%);
   }
 `
 
@@ -209,7 +162,7 @@ export const ContactForm = styled.form`
 
   textarea {
     margin: 10px 0;
-    height: 30vh;
+    height: 25vh;
     border: none;
     border-radius: 0 0 10px 10px;
     overflow: hidden;
