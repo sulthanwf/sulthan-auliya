71dec739618d9af5fa9f327d3461695c553d7425
sulthan <sulthanwf@gmail.com>
2021-06-29 15:01:19 +1200

    modify gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,13 +40,7 @@ module.exports = {
       resolve: "gatsby-plugin-firebase",
       options: {
         credentials: {
-          apiKey: "AIzaSyBSZX7euFaAJR6dqfAgabrKNXtz5NUQCmY",
-          authDomain: "sulthanauliya-93b5b.firebaseapp.com",
-          projectId: "sulthanauliya-93b5b",
-          storageBucket: "sulthanauliya-93b5b.appspot.com",
-          messagingSenderId: "928005354940",
-          appId: "1:928005354940:web:170e9e9493bf2f2c19e7b9",
-          measurementId: "G-JGZ3032DSX",
+          //Your API Key
         },
       },
     },
